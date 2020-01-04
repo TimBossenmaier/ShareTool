18c4a960270c91801343da0aa17a496f17a174b8
Tim <timbo95+github@posteo.de>
2020-01-04 17:17:51 +0100

    changed definition of SERIAL and file enumeration, corrected typo

---
+++ data/sql/07_init_data-assetTurnovers.sql
@@ -0,0 +1,24 @@
+CREATE TABLE data."assetTurnovers"
+(
+	"ID" SERIAL NOT NULL,
+	share integer NOT NULL,
+	year integer NOT NULL,
+	asset_turnover double precision NOT NULL,
+	valid_from timestamp(4) without time zone NOT NULL,
+	valid_to timestamp(4) without time zone NOT NULL,
+	CONSTRAINT "assetTurnovers_pkey" PRIMARY KEY ("ID"),
+	CONSTRAINT "assetTurnovers_share_fkey" FOREIGN KEY (share)
+		REFERENCES entities.shares("ID") MATCH SIMPLE
+		ON UPDATE NO ACTION
+		ON DELETE NO ACTION
+		NOT VALID
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE data."assetTurnovers"
+	OWNER TO postgres;
+
+GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE data."assetTurnovers" TO db_tool_user;
+
+GRANT ALL ON TABLE data."assetTurnovers" TO postgres;
