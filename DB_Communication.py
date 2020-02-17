bdb33a82fa15bce2a9842946de8798955dcad3da
Tim B <timbo95+github@posteo.de>
2020-02-17 19:56:49 +0100

    implement insert_cashflows() in DBCommunication

--- DB_Communication.py
+++ DB_Communication.py
@@ -508,3 +508,35 @@ def insert_profits(db_connection, values):
 
     return error_message
 
+
+def insert_cashflows(db_connection, values):
+    """
+    Performs insert statement of cashflow entries
+    :param db_connection: psycopg2 connection to database
+    :param values: dictionary of values to be integrated in the statement
+    :return: error message
+    """
+
+    # TODO: combine all insert methods to one
+
+    error_message = None
+    try:
+        sql_cursor = db_connection.cursor()
+
+        column_names = get_column_names_from_db_table(sql_cursor, "cashflows")
+
+        # remove ID as this is generated automatically by the database
+        column_names.remove("ID")
+
+        query = create_insert_into_statement("cashflows", column_names)
+
+        for i in range(len(values["year"])):
+            sql_cursor.execute(query, (values["year"][i], values["share_ID"][i], values["profit"][i],
+                                       values["valid_from"][i], values["valid_to"][i]))
+
+        db_connection.commit()
+
+    except BaseException as e:
+        error_message = e
+
+    return error_message
