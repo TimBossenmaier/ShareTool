129599024999e29ebf40aee735b69c58e6483ce5
Tim B <timbo95+github@posteo.de>
2020-02-26 17:59:14 +0100

    adapt to leverage table

--- DB_Communication.py
+++ DB_Communication.py
@@ -369,10 +369,14 @@ def get_data_for_specific_share(sql_cursor, share_id, table_name):
     :param table_name: name of the corresponding db table
     :return:
     """
-
-    sql_query = 'SELECT tab.year, tab.' + table_name[:-1] + ' FROM entities.shares share ' \
-                'INNER JOIN data."' + table_name + '" tab on tab."share_ID" = share."ID" ' \
-                'WHERE share."ID" = ' + str(share_id)
+    if table_name == 'leverages':
+        sql_query = 'SELECT tab.year, tab.' + '	debt_to_equity_ratio' + ' FROM entities.shares share ' \
+                    'INNER JOIN data."' + table_name + '" tab on tab."share_ID" = share."ID" ' \
+                    'WHERE share."ID" = ' + str(share_id)
+    else:
+        sql_query = 'SELECT tab.year, tab.' + table_name[:-1] + ' FROM entities.shares share ' \
+                    'INNER JOIN data."' + table_name + '" tab on tab."share_ID" = share."ID" ' \
+                    'WHERE share."ID" = ' + str(share_id)
 
     sql_cursor.execute(sql_query)
 
