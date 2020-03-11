b1940af30502949c9d8cf87f711e8ebdd4f66ecc
Tim B <timbo95+github@posteo.de>
2020-03-11 10:53:53 +0100

    implement insert_data_in_db for leverage Page

--- GUI.py
+++ GUI.py
@@ -386,7 +386,6 @@ class ShareToolGUI(tk.Tk):
             self.show_frame(InsertLeveragePage)
 
 
-
 class BasicPage(tk.Frame):
     """
     Super class for all pages
@@ -1696,7 +1695,7 @@ class InsertROAPage(ParentInsertPage):
             ts_current_time = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
             # create a dictionary with all values for each year
-            values_per_entry.update({"year": list([y for y, p in values_to_be_inserted ])})
+            values_per_entry.update({"year": list([y for y, p in values_to_be_inserted])})
             values_per_entry.update({"share_ID": list([self.current_share_id for i in range(len(values_to_be_inserted))
                                                        ])})
             values_per_entry.update({self.insert_type: list([p for y, p in values_to_be_inserted])})
@@ -1783,3 +1782,119 @@ class InsertLeveragePage(ParentInsertPage):
         # reset all spinboxes
         self.spinbox_var_1.set(value=self.create_five_year_range()[-1])
         self.spinbox_var_2.set(value=self.create_five_year_range()[-2])
+
+    def insert_data_in_db(self):
+        """
+        Perform several validity checks for the user input.
+        If no errors are detected, create the inserted leverage values in the database.
+        :return: None
+        """
+
+        errors_detected = True
+
+        # get the current share id selected in the combobox
+        try:
+            self.current_share_id = self.df_shares.ID[self.df_shares.company_name == self.combobox_shares.get()].iloc[0]
+        except IndexError:
+            messagebox.showerror(title="No selection",
+                                 message="No combobox item selected! \n" 
+                                         "Please select a share to which the leverage value should refer.")
+            errors_detected = True
+
+        # get the inserted leverage values
+        leverage_1 = self.entry_leverage_1.get()
+        leverage_2 = self.entry_leverage_2.get()
+
+        values_to_be_inserted = []
+
+        # control for each checkbox whether inputs are valid and consistent
+        # - if a checkbox is selected the corresponding leverage value has to be inserted
+        # - the inserted leverage value has to be a float
+        if self.checkbox_1_selected.get() and leverage_1 == '' and not errors_detected:
+            messagebox.showerror(title="Missing leverages",
+                                 message="Leverage input is empty, \n" 
+                                         "Please specify a leverage value or toggle a checkbox.")
+            errors_detected = True
+
+        if self.checkbox_1_selected.get()  and leverage_1 != '' and not errors_detected:
+            try:
+                values_to_be_inserted.append((self.spinbox_var_1.get(), float(leverage_1)))
+            except ValueError:
+                messagebox.showerror(title="Value Error",
+                                     message="Please insert a number as leverage")
+                errors_detected = True
+
+        if self.checkbox_2_selected.get() and leverage_2 == '' and not errors_detected:
+            messagebox.showerror(title="Missing leverages",
+                                 message="Leverage input is empty, \n"
+                                         "Please specify a leverage value or toggle a checkbox.")
+            errors_detected = True
+
+        if self.checkbox_2_selected and leverage_2 != '' and not errors_detected:
+            try:
+                values_to_be_inserted.append((self.spinbox_var_2.get(), float(leverage_2)))
+            except ValueError:
+                messagebox.showerror(title="Value Error",
+                                     message="Please insert a number as leverage")
+                errors_detected = True
+
+        # show message if none of the checkboxes are selected
+        if not self.checkbox_1_selected and \
+           not self.checkbox_2_selected and \
+           not errors_detected:
+            messagebox.showerror(title="Empty Statement",
+                                 message="None of the checkboxes are selected. \n"
+                                         "Accordingly, no values will be inserted. \n"
+                                         "Please select al least one combobox.")
+            errors_detected = True
+
+        # get list of years for which leverage values are already in the database
+        list_existing_years = DB_Communication.get_years_for_specific_share(self.db_connection.cursor(),
+                                                                            self.insert_type + "s",
+                                                                            self.current_share_id)
+        list_duplicated_years = []
+
+        # catch each year which has already a leverage value
+        for y, p in values_to_be_inserted:
+            if y in list_existing_years:
+                list_duplicated_years.append(y)
+
+        # show message which years are already existent
+        if len(list_duplicated_years) > 0 and not errors_detected:
+            message_text = "Leverage(s) for "
+
+            for each_year in list_duplicated_years:
+                message_text += str(each_year) + " "
+
+            message_text += "already exist(s). \nPlease use Update section to change the values."
+            messagebox.showerror(title="Year(s) exist already",
+                                 message=message_text)
+            errors_detected = True
+
+        values_per_entry = {}
+
+        # if no errors are found so far, we can insert the values in the database
+        if not errors_detected:
+
+            ts_current_time = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+            # create a dictionary with all values for each year
+            values_per_entry.update({"year": list([y for y, p in values_to_be_inserted])})
+            values_per_entry.update({"share_ID": list([self.current_share_id for i in range(len(values_to_be_inserted))
+                                                       ])})
+            values_per_entry.update({self.insert_type:  list([p for y, p in values_to_be_inserted])})
+            values_per_entry.update({"valid_from": list([ts_current_time for i in range(len(values_to_be_inserted))])})
+            values_per_entry.update({"valid_to": list(['9999-12-31 23:59:59' for i in range(len(values_to_be_inserted))
+                                                       ])})
+
+            # finally perform insert into db
+            error = DB_Communication.insert_leverages(self.db_connection, values_per_entry)
+
+            if error is None:
+                self.update_frame()
+                messagebox.showinfo(title="Success!",
+                                    message="The configured has been successfully created in the database.")
+            else:
+                messagebox.showerror(title="DB Error",
+                                     message="An error has occurred. Please try again."
+                                             "In case the error remains, please restart the application")
